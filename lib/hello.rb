ad808b5bba118c0bf30b40f205bf30aaa602d77f
Darrel C <dcastellano2007@gmail.com>
2019-09-10 04:22:51 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,6 +5,7 @@ def hello_t(array)
     yeild array[i]
     i = i + 1
   end
+  array
 end
 
 # call your method here!
@@ -12,4 +13,5 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
+
 end
